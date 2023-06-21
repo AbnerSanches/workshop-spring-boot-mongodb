2fd228330ded4edcabf226ab74d933421a8f9f37
AbnerSanches <abnerisrael678803@gmail.com>
2023-06-21 18:28:17 -0300

    Conectando ao MongoDB com repositoy e service

---
+++ src/main/java/com/abner/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.abner.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.abner.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
