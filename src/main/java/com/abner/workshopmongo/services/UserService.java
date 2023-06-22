02ce4eed5c5a6148864d5ee866f77011176757af
AbnerSanches <abnerisrael678803@gmail.com>
2023-06-22 17:13:11 -0300

    Obtendo um usuário por id

--- src/main/java/com/abner/workshopmongo/services/UserService.java
+++ src/main/java/com/abner/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.abner.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.abner.workshopmongo.domain.User;
 import com.abner.workshopmongo.repository.UserRepository;
+import com.abner.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
