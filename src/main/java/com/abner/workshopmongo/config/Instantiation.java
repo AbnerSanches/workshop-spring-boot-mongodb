917943e73823a2885ccdc24d216966ae9d553425
AbnerSanches <abnerisrael678803@gmail.com>
2023-06-25 10:56:51 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/abner/workshopmongo/config/Instantiation.java
+++ src/main/java/com/abner/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.abner.workshopmongo.domain.Post;
 import com.abner.workshopmongo.domain.User;
 import com.abner.workshopmongo.dto.AuthorDTO;
+import com.abner.workshopmongo.dto.CommentDTO;
 import com.abner.workshopmongo.repository.PostRepository;
 import com.abner.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Patiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+		post1.getComents().addAll(Arrays.asList(c1, c2));
+		post2.getComents().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
